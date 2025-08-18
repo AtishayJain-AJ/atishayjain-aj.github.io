d3d7995b6728abd7de29331005df4546ea3506b3
AtishayJain-AJ <atishay_jain@brown.edu>
2025-08-18 18:35:33 +0000

    Deploying to gh-pages from @ AtishayJain-AJ/atishayjain-aj.github.io@72391814ff135ce783177076ac9549146630b431 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-presented-our-work-improved-spatial-transcriptomics-clustering-with-nested-graph-neural-networks-as-a-poster-at-the-ismb-2024",
+          title: 'Presented our work “Improved Spatial Transcriptomics Clustering with Nested Graph Neural Networks” as...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-presented-our-work-improved-spatial-transcriptomics-clustering-with-nested-graph-neural-networks-as-a-poster-at-the-cshl-biological-data-science-meeting",
+          title: 'Presented our work “Improved Spatial Transcriptomics Clustering with Nested Graph Neural Networks” as...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-our-paper-titled-improved-spatial-transcriptomics-clustering-with-nested-graph-neural-networks-was-accepted-at-acm-bcb-2025",
+          title: 'Our paper titled “Improved Spatial Transcriptomics Clustering with Nested Graph Neural Networks” was...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -459,35 +457,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%74%69%73%68%61%79_%6A%61%69%6E@%62%72%6F%77%6E.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/AtishayJain-AJ", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/atishay-jain-511950121", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-9972-4217", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=WFYvgdcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
